4dd860fcab7c57df8ea6474a69a2f6e4206bf7fd
AL-AMIN <32561876+Alamin999bd@users.noreply.github.com>
2018-01-10 19:38:41 +0600

    Add files via upload

---
+++ Sum of N number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    printf("Enter the number of N:\n");
+    scanf("%d",&n);
+    for(i=0; i<=n; i++)
+    {
+        sum=sum+i;
+
+    }
+
+    printf("The sum is =%d",sum);
+    getch();
+
+}
